8457363c7b711b6031decb59aade06a8cccd460e
awaaawaa <146153699+awaaawaa@users.noreply.github.com>
2024-03-12 23:42:34 +0700

    Add files via upload

---
+++ week 3/praktek/looping.php
@@ -0,0 +1,18 @@
+<?php
+//looping dengan for
+//pengukangan dari 1-10
+for($x=1; $x<=10; $x=$x+1){
+    echo '<br>'.$x;
+}
+
+//pengulangan dari 1-10
+for($y=10; $y>=1; $y=$y-1){
+    echo '<br>'.$y;
+}
+
+//pengulangan teks
+for($x=1; $x<=10; $x=$x+1){
+    echo "<br>awa cakep..";
+}
+
+?>
